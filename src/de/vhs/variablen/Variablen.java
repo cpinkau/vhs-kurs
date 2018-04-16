0c4c9c6e19cffdde7ebff6f160aaedd724ed8b62
Chris Pinkau <chris.pinkau@tngtech.com>
2018-04-16 22:09:18 +0200

    Dateien der ersten Woche

---
+++ src/de/vhs/variablen/Variablen.java
@@ -0,0 +1,20 @@
+package de.vhs.variablen;
+
+public class Variablen {
+
+    public static void main(String[] args) {
+        int tag;
+        boolean dieSonneScheint;
+        double kommazahl;
+
+        tag = 16;
+        dieSonneScheint = true;
+        kommazahl = 7.52;
+
+        System.out.println(tag);
+        System.out.println("Es scheint die Sonne:");
+        System.out.println(dieSonneScheint);
+        System.out.println(kommazahl);
+    }
+
+}
---
+++ src/de/vhs/variablen/VariablenTest.java
@@ -0,0 +1,17 @@
+package de.vhs.variablen;
+
+public class VariablenTest {
+
+    public static void main(String[] args) {
+        String name = "Chris";
+        String farbe = "blau";
+        int zahl = 42;
+
+        System.out.println("Mein Name ist:");
+        System.out.println(name);
+        System.out.println("Meine Lieblingsfarbe ist:");
+        System.out.println(farbe);
+        System.out.println("Meine Lieblingszahl ist:");
+        System.out.println(zahl);
+    }
+}
